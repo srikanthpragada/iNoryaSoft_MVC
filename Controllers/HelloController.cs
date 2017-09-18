1b7c5ed3c7446d7338c8192bbbef4a323ecd32cd
srikanthpragada <srikanthpragada@gmail.com>
2017-09-18 13:06:23 +0530

    18-SEP

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewBag.Today = DateTime.Now.ToLongDateString();
+            ViewBag.Title = "Index of Hello";
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "ASP.NET MVC Training";
+            return View();
+        }
+
+        // [Route("/Company")]
+        public ActionResult Company()
+        {
+            ViewBag.Message = "iNoryaSoft";
+            return View();
+        }
+    }
+}
